c66c8cccfc322d1347ab216515755289402fa892
Matthias Brown <matthias.brown@epita.fr>
2021-01-17 13:03:23 +0100

    refactor: Add AnimationUtils

---
+++ app/src/main/java/com/example/app_template_java/views/utils/AnimationUtils.java
@@ -0,0 +1,180 @@
+package com.example.app_template_java.views.utils;
+
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.TimeInterpolator;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class AnimationUtils {
+
+    public final static long ANIMATION_DURATION = 300L;
+    public final static long ANIMATION_DELAY = 100L;
+
+    public final static class Builder {
+
+        private List<Object> objects = new ArrayList<>();
+
+        private boolean animateAlphaIn = false;
+        private boolean animateAlphaOut = false;
+
+        private boolean animateScaleUp = false;
+        private boolean animateScaleDown = false;
+
+        private long duration = ANIMATION_DURATION;
+        private long delay = ANIMATION_DELAY;
+
+        private TimeInterpolator interpolator = null;
+
+        private float translationYBegin = 0f;
+        private float translationYEnd = 0f;
+
+        private float translationXBegin = 0f;
+        private float translationXEnd = 0f;
+
+        public Builder() {
+
+        }
+
+        public Builder setObjects(List<Object> objects) {
+            this.objects = objects;
+            return this;
+        }
+
+        public Builder setAnimateAlphaIn(boolean animateAlphaIn) {
+            this.animateAlphaIn = animateAlphaIn;
+            return this;
+        }
+
+        public Builder setAnimateAlphaOut(boolean animateAlphaOut) {
+            this.animateAlphaOut = animateAlphaOut;
+            return this;
+        }
+
+        public Builder setAnimateScaleUp(boolean animateScaleUp) {
+            this.animateScaleUp = animateScaleUp;
+            return this;
+        }
+
+        public Builder setAnimateScaleDown(boolean animateScaleDown) {
+            this.animateScaleDown = animateScaleDown;
+            return this;
+        }
+
+        public Builder setDuration(long duration) {
+            this.duration = duration;
+            return this;
+        }
+
+        public Builder setDelay(long delay) {
+            this.delay = delay;
+            return this;
+        }
+
+        public Builder setInterpolator(TimeInterpolator interpolator) {
+            this.interpolator = interpolator;
+            return this;
+        }
+
+        public Builder setTranslationY(float begin, float end) {
+            this.translationYBegin = begin;
+            this.translationYEnd = end;
+            return this;
+        }
+
+        public Builder setTranslationYBegin(float begin) {
+            this.translationYBegin = begin;
+            return this;
+        }
+
+        public Builder setTranslationYEnd(float end) {
+            this.translationYEnd = end;
+            return this;
+        }
+
+        public Builder setTranslationX(float begin, float end) {
+            this.translationXBegin = begin;
+            this.translationXEnd = end;
+            return this;
+        }
+
+        public Builder setTranslationXBegin(float translationXBegin) {
+            this.translationXBegin = translationXBegin;
+            return this;
+        }
+
+        public Builder setTranslationXEnd(float translationXEnd) {
+            this.translationXEnd = translationXEnd;
+            return this;
+        }
+
+        public void start() {
+
+            List<Animator> animators = new ArrayList<>();
+            for (Object object: objects) {
+                ObjectAnimator alpha = null;
+                float begin = -1f, end = -1f;
+
+                if (animateAlphaIn) {
+                    begin = 0f;
+                    end = 1f;
+                } else if (animateAlphaOut) {
+                    begin = 1f;
+                    end = 0f;
+                }
+
+                if (begin != -1f && end != -1f) {
+                    alpha = ObjectAnimator.ofFloat(object, "alpha", begin, end);
+                    animators.add(alpha);
+                }
+
+                ObjectAnimator scaleX, scaleY;
+                begin = -1f;
+                end = -1f;
+                if (animateScaleUp) {
+                    begin = 0f;
+                    end = 1f;
+                } else if (animateScaleDown) {
+                    begin = 1f;
+                    end = 0f;
+                }
+
+                if (begin != -1f && end != -1f) {
+                    scaleX = ObjectAnimator.ofFloat(object, "scaleX", begin, end);
+                    scaleY = ObjectAnimator.ofFloat(object, "scaleY", begin, end);
+                    animators.add(scaleX);
+                    animators.add(scaleY);
+                }
+
+                if (translationYBegin != 0f || translationYEnd != 0f) {
+                    ObjectAnimator translateY = ObjectAnimator.ofFloat(object, "translationY", translationYBegin, translationYEnd);
+                    animators.add(translateY);
+                }
+
+                if (translationXBegin != 0f || translationXEnd != 0f) {
+                    ObjectAnimator translateX = ObjectAnimator.ofFloat(object, "translationX", translationXBegin, translationXEnd);
+                    animators.add(translateX);
+                }
+
+            }
+
+            animate(animators, duration, delay, interpolator);
+
+        }
+
+    }
+
+    public static void animate(List<Animator> animators, long duration, long delay, TimeInterpolator interpolator) {
+        Collection<Animator> animatorCollection = animators;
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(animatorCollection);
+        animatorSet.setInterpolator(interpolator);
+        animatorSet.setDuration(duration);
+        animatorSet.setStartDelay(delay);
+        animatorSet.start();
+    }
+
+}
